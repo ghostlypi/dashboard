35e93f450b1a2cf32ccc9746282663eef556927f
spiderfencer1 <jinjoo@gmail.com>
2017-11-01 13:50:04 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -267,9 +267,10 @@ window.onload = () =>
 	
 	id('foreground').onkeydown = function(e){
 		if(e.keyCode === 13){
-				[].forEach.call(cls('clr-fg'),function(f){f.style.color = id('foreground').value;})
-				e.preventDefault();
-			}
+			[].forEach.call(cls('clr-fg'),function(f){f.style.color = id('foreground').value;})
+			[].forEach.call(cls('clr-bg'),function(f){f.style.backgroundColor = id('foreground').value;})
+			e.preventDefault();
+		}
 	};
 	
 	id('signin').onclick = function(){
