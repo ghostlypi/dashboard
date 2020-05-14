4a547e09ab589ef703e42363d9faf814f0dd2804
spiderfencer1 <jinjoo@gmail.com>
2020-05-14 10:29:24 -0700

    fixed icons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 window.onload = () =>
 {
-	var user = {
-		'email':''
-	};
+	var user = {};
 
 	// Your web app's Firebase configuration
 	var firebaseConfig = {
@@ -68,6 +66,9 @@ window.onload = () =>
 	{
 		const elem = cls('percent')[[].indexOf.call(cls('add'),e)];
 		elem.value = Math.min(parseInt(elem.value) + 1,100);
+		const idx = [].indexOf.call(cls('percent'), elem);
+		user.goals[idx]['progress'] = elem.value;
+		firebase.firestore().collection('users').doc(user.email).set(user);
 	});
 
 	[].forEach.call(cls('sub'),
@@ -75,6 +76,18 @@ window.onload = () =>
 	{
 		const elem = cls('percent')[[].indexOf.call(cls('sub'),e)];
 		elem.value = Math.max(parseInt(elem.value) - 1,0);
+		const idx = [].indexOf.call(cls('percent'), elem);
+		user.goals[idx]['progress'] = elem.value;
+		firebase.firestore().collection('users').doc(user.email).set(user);
+	});
+
+	[].forEach.call(cls('goal'),
+		e => e.onkeydown = evt =>
+	{
+		if(evt.keyCode === 13) { evt.preventDefault(); }
+		const idx = [].indexOf.call(cls('goal'), e);
+		user.goals[idx]['name'] = e.value;
+		firebase.firestore().collection('users').doc(user.email).set(user);
 	});
 
 	[].forEach.call(cls('percent'),
@@ -123,8 +136,7 @@ window.onload = () =>
 	{
 		setInterval(() =>
 		{
-			let disp = cls('percent')
-			[[].indexOf.call
+			let disp = cls('percent')[[].indexOf.call
 			(
 				cls('prog-bar'),
 				elem
@@ -196,16 +208,24 @@ window.onload = () =>
   load();
   setInterval(_ => save(), 1000);
 
-	const tasks = [0,0,0,0,0,0,0,0,0,0];
+	const tasks = ['0','0','0','0','0','0','0','0','0','0'];
+
+	[].forEach.call(cls('task-name'),
+		elem => elem.onkeydown = ev =>
+	{
+		const idx = [].indexOf.call(cls('task-name'),elem);
+		user.tasks[idx]['name'] = cls('task-name')[idx].value;
+		firebase.firestore().collection('users').doc(user.email).set(user);
+		if(ev.keyCode === 13) { ev.preventDefault(); }
+	});
 
 	[].forEach.call(cls('task-start'),
 		elem => elem.onclick = () =>
 	{
 		const idx = [].indexOf.call(cls('task-start'),elem);
-		let i = 0;
+		if(typeof tasks[idx] !== 'string') { return; }
 		tasks[idx] = setInterval(() =>
 		{
-			i++;
       if (cls('task-time')[idx].value === "") {
         cls('task-time')[idx].value = "00:10:00";
       }
@@ -213,6 +233,7 @@ window.onload = () =>
 			let h = dt[0];
 			let m = dt[1];
 			let s = dt[2] - 1;
+			firebase.firestore().collection('users').doc(user.email).set(user);
 			if(s < 0) {
 				m--;
 				s = 59;
@@ -230,6 +251,7 @@ window.onload = () =>
 			m = parseLen(m);
 			s = parseLen(s);
 			cls('task-time')[idx].value = h + ':' + m + ':' + s;
+			user.tasks[idx]['progress'] = h + ':' + m + ':' + s;
 		},1000);
 	});
 
@@ -238,6 +260,7 @@ window.onload = () =>
 	{
 		const idx = [].indexOf.call(cls('task-stop'),elem);
 		clearInterval(tasks[idx]);
+		tasks[idx] = '0';
 	});
 
 	let ctrl = false;
@@ -255,6 +278,10 @@ window.onload = () =>
 	[].forEach.call(cls('background'),function(c){
 		c.onkeydown = function(e){
 			if(e.keyCode === 13){
+				user.backgroundColor = id('background-1').value;
+				user.mainBoxColor = id('background-2').value;
+				user.subBoxColor = id('background-3').value;
+				firebase.firestore().collection('users').doc(user.email).set(user);
 				tag('html')[0].style.setProperty('--bg-color-a',id('background-1').value);
 				tag('html')[0].style.setProperty('--bg-color-b',id('background-2').value);
 				tag('html')[0].style.setProperty('--bg-color-c',id('background-3').value);
@@ -263,7 +290,14 @@ window.onload = () =>
 		}
 	});
 
+	id('notes').onkeydown = function() {
+		user.notes = id('notes').value;
+		firebase.firestore().collection('users').doc(user.email).set(user);
+	}
+
 	id('foreground').onkeydown = function(e){
+		user.textColor = id('foreground').value;
+		firebase.firestore().collection('users').doc(user.email).set(user);
 		if(e.keyCode === 13){
 				[].forEach.call(cls('clr-fg'),function(f){f.style.color = id('foreground').value;});
 				[].forEach.call(cls('clr-bg'),function(f){f.style.backgroundColor = id('foreground').value;});
@@ -275,9 +309,43 @@ window.onload = () =>
 		var email = id('email').value;
 		var passw = id('passw').value;
 		firebase.auth().createUserWithEmailAndPassword(email, passw).then(function() {
-			var temp_user = {'username':'Username'};
+			var temp_user = {
+				'username':'Username',
+				'email':email,
+				'goals':[
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0},
+					{'name':'','progress':0}
+				],
+				'imageName':'',
+				'notes':'',
+				'backgroundColor':'',
+				'mainBoxColor':'',
+				'subBoxColor':'',
+				'textColor':'',
+				'shadows':true,
+				'tasks':[
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''},
+					{'name':'','progress':''}
+				],
+			};
 			firebase.firestore().collection('users').doc(email).set(temp_user);
-			user.email = email;
+			user = temp_user;
 			id('signin').style.visibility = 'hidden';
 			id('dashboard').style.visibility = 'visible';
 			[].forEach.call(id('dashboard').children,function(c){c.style.visibility = 'visible';});
@@ -290,7 +358,71 @@ window.onload = () =>
 		var email = id('email').value;
 		var passw = id('passw').value;
 		firebase.auth().signInWithEmailAndPassword(email, passw).then(function() {
-			user.email = email;
+			firebase.firestore().collection('users').doc(email).get().then(function(response) {
+				if(response.exists) {
+					user = response.data();
+					id('username').innerHTML = user.username;
+					[].forEach.call(cls('goal'), function(goal) {
+						const idx = [].indexOf.call(cls('goal'), goal);
+						if(idx < user.goals.length) {
+							goal.value = user.goals[idx].name;
+						}
+					});
+					[].forEach.call(cls('percent'), function(percent) {
+						const idx = [].indexOf.call(cls('percent'), percent);
+						if(idx < user.goals.length) {
+							percent.value = '' + user.goals[idx].progress;
+							var elem = [...cls('prog-bar')][idx];
+							if(user.goals[idx].progress <= 33.33)
+							{
+								const pX = 50 + 45 * user.goals[idx].progress / 33.33;
+								const pY = 5 + 90 * user.goals[idx].progress / 33.33;
+								elem.setAttribute('d','M50 5 L' + pX + ' ' + pY + ' Z');
+							}
+							else if(user.goals[idx].progress <= 66.66)
+							{
+								const pX = 95 - 90 * (user.goals[idx].progress - 33.33) / 33.33;
+								elem.setAttribute('d','M50 5 L95 95 M95 95 L' + pX + ' 95 Z');
+							}
+							else
+							{
+								const pX = 5 + 45 * (user.goals[idx].progress - 66.66) / 33.33;
+								const pY = 95 - 90 * (user.goals[idx].progress - 66.66) / 33.33;
+								elem.setAttribute('d','M50 5 L95 95 M95 95 L5 95 M5 95 L'
+									+ pX + ' ' + pY + ' Z');
+							}
+						}
+					});
+					id('notes').value = user.notes;
+					id('background-1').value = user.backgroundColor;
+					id('background-2').value = user.mainBoxColor;
+					id('background-3').value = user.subBoxColor;
+					tag('html')[0].style.setProperty('--bg-color-a',id('background-1').value);
+					tag('html')[0].style.setProperty('--bg-color-b',id('background-2').value);
+					tag('html')[0].style.setProperty('--bg-color-c',id('background-3').value);
+					id('shadows').firstChild.data = user.shadows ? 'Off' : 'On';
+					if(id('shadows').firstChild.data === 'Off'){
+						[].forEach.call(cls('cell'),function(c){c.style.boxShadow = 'none';});
+					}else{
+						[].forEach.call(cls('cell'),function(c){c.style.boxShadow = '5px 5px rgba(0, 0, 0, 0.3)';});
+					}
+					[].forEach.call(cls('task-name'), function(taskName) {
+						const idx = [].indexOf.call(cls('task-name'), taskName);
+						if(idx < user.tasks.length) {
+							taskName.value = user.tasks[idx].name;
+						}
+					});
+					[].forEach.call(cls('task-time'), function(taskTime) {
+						const idx = [].indexOf.call(cls('task-time'), taskTime);
+						if(idx < user.tasks.length) {
+							taskTime.value = user.tasks[idx].progress;
+						}
+					});
+					firebase.storage().ref().child(user.imageName).getDownloadURL().then(function(url) {
+						id('profile-image').src = url;
+					});
+				}
+			});
 			id('signin').style.visibility = 'hidden';
 			id('dashboard').style.visibility = 'visible';
 			[].forEach.call(id('dashboard').children,function(c){c.style.visibility = 'visible';});
@@ -308,6 +440,8 @@ window.onload = () =>
 	*/
 
 	id('shadows').onclick = function(){
+		user.shadows = !user.shadows;
+		firebase.firestore().collection('users').doc(user.email).set(user);
 		id('shadows').firstChild.data = id('shadows').firstChild.data === 'On' ? 'Off' : 'On';
 		if(id('shadows').firstChild.data === 'Off'){
 			[].forEach.call(cls('cell'),function(c){c.style.boxShadow = 'none';});
@@ -323,6 +457,9 @@ window.onload = () =>
 	id('username').onkeydown = function(ev) {
 		if(ev.keyCode === 13) {
 			ev.preventDefault();
+			user.username = id('username').innerHTML;
+			console.log(user);
+			firebase.firestore().collection('users').doc(user.email).set(user);
 			id('username').contentEditable = false;
 		}
 	};
@@ -334,7 +471,7 @@ window.onload = () =>
 	};
 
 	id('user-logout').onclick = function() {
-		user.email = '';
+		user = {};
 		id('email').value = '';
 		id('passw').value = '';
 		firebase.auth().signOut();
@@ -349,6 +486,16 @@ window.onload = () =>
 			id('profile-image').src = e.target.result;
 		};
 		reader.readAsDataURL(ev.target.files[0]);
+		firebase.firestore().collection('num_images').doc('num_images').get().then(function(response) {
+			if(response.exists) {
+				var num_images = response.data();
+				user.imageName = 'images/' + num_images.num_images.toString() + '.png';
+				firebase.storage().ref().child(user.imageName).put(ev.target.files[0]);
+				firebase.firestore().collection('users').doc(user.email).set(user);
+				num_images.num_images++;
+				firebase.firestore().collection('num_images').doc('num_images').set(num_images);
+			}
+		});
 	}
 
 	id('profile-image').onclick = function() {
